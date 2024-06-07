c045284b3fa9945ccb48192d9c186598cebe912e
Tejoyasaswi-17 <tejoyasaswi1703@gmail.com>
2024-06-07 15:56:45 +0530

    8. grid lights (no useeffecrt)

--- src/components/grid-lights/index.js
+++ src/components/grid-lights/index.js
@@ -1,59 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.css';
 
 const GridLights = () => {
-    const [squares, setSquares] = useState([
+    const config = [
         [0, 0, 0],
         [0, -1, 0],
         [0, 0, 0]
-    ]);
+    ];
 
-    const [filledSquares, setFilledSquares] = useState([]);
-    const [gameOver, setGameOver] = useState(false);
+    const [filledSquaresOrder, setFilledSquaresOrder] = useState([]);
 
-    const handleClick = (index1, index2) => {
-        const newSquares = [...squares];
-        newSquares[index1][index2] = 1;
-        setSquares(newSquares);
-        setFilledSquares((prev) => [[index1, index2], ...prev]);
+    const deactiveCells = () => {
+        const timer = setInterval(() => {
+            setFilledSquaresOrder((prev) => {
+                const newFilledSquareOrder = prev.slice(1);
+                if (newFilledSquareOrder.length === 0) {
+                    clearInterval(timer);
+                }
+                return newFilledSquareOrder;
+            })
+            
+        }, 300);
+    }
+
+    const activateCell = (index1, index2) => {
+        const newFilledSquares = [[index1, index2], ...filledSquaresOrder];
+        setFilledSquaresOrder((prev) => [[index1, index2], ...prev]);
+        if (newFilledSquares.length === 8) {
+            deactiveCells();
+        }
+
     };
-
-    useEffect(() => {
-        if (filledSquares.length === 8) {
-            setGameOver(true);
-        }
-        if (filledSquares.length === 0) {
-            setGameOver(false);
-        }
-    }, [filledSquares]);
-
-
-    useEffect(() => {
-        if (gameOver && filledSquares.length > 0) {
-            setTimeout(() => {
-                const newConfig = [...squares];
-                const [i, j] = filledSquares[0];
-                newConfig[i][j] = 0;
-                setSquares(newConfig);
-                setFilledSquares((prev) => prev.slice(1));
-            }, 300);
-        }
-    }, [gameOver, filledSquares]);
 
     return (
         <div className={styles.container}>
             <span>Grid Lights</span>
             <div className={styles.grid}>
-                {squares.map((item1, index1) => {
+                {config.map((item1, index1) => {
                     return <div className={styles.block} key={index1}>
                         {item1.map((item2, index2) => {
                             if (item2 === -1) {
                                 return null;
                             }
+                            const isActivated = filledSquaresOrder.some(item => JSON.stringify(item) ===
+                                JSON.stringify([index1, index2]));
+                            
                             return (<div
-                                className={item2 === 0 ? styles.ind_block : styles.filled_block}
+                                className={isActivated ? styles.filled_block : styles.ind_block}
                                 key={index2}
-                                onClick={() => handleClick(index1, index2)}
+                                onClick={() => activateCell(index1, index2)}
                             >
                             </div>);
                         })}
